9c4ad5136de78b248e300bbc48d59bc7d140027b
Symon Hasan <isymonhs@gmail.com>
2020-08-18 22:14:42 +0600

    Navigation component design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
