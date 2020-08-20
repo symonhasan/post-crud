da2f0335d68d031fdb419a8f75907199f40ed883
Symon Hasan <isymonhs@gmail.com>
2020-08-20 21:17:28 +0600

    Updates

---
+++ precache-manifest.e241851284487c9d156cfab196f8d5bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb67ed16b93060bf65ba97db3d339b7d",
+    "url": "/post-crud/index.html"
+  },
+  {
+    "revision": "eb60a2728b6379f8e3a8",
+    "url": "/post-crud/static/css/main.795861e0.chunk.css"
+  },
+  {
+    "revision": "9555178081e6c4f5f392",
+    "url": "/post-crud/static/js/2.352b1085.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/post-crud/static/js/2.352b1085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb60a2728b6379f8e3a8",
+    "url": "/post-crud/static/js/main.4e2b38b6.chunk.js"
+  },
+  {
+    "revision": "57f9c7bbac775c295fa0",
+    "url": "/post-crud/static/js/runtime-main.960e5573.js"
+  },
+  {
+    "revision": "2febd8c3538346f2ccea7f4bfc142792",
+    "url": "/post-crud/static/media/down-chevron.2febd8c3.svg"
+  },
+  {
+    "revision": "8b4d6e74cffa8b09d37d84376cd22e12",
+    "url": "/post-crud/static/media/tick.8b4d6e74.svg"
+  }
+]);
